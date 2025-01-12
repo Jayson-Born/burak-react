f2b0b018318869d56e02fff8e8125bc857d126b2
Jayson-Born <abdukabir34@gmail.com>
2025-01-12 23:31:01 +0900

    feat; create routing system

---
+++ src/app/screens/About.tsx
@@ -0,0 +1,6 @@
+import Container from "@mui/material/Container/Container";
+import React from "react";
+
+export function About(){
+    return <Container>About</Container>;
+}
---
+++ src/app/screens/Users.tsx
@@ -0,0 +1,6 @@
+import Container from "@mui/material/Container/Container";
+import React from "react";
+
+export function Users(){
+    return <Container>Users</Container>;
+}
