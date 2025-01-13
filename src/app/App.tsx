d7133bfc4da0d5875a87407851a23e55761c2f23
Jayson-Born <abdukabir34@gmail.com>
2025-01-13 20:30:45 +0900

    fix: orgonize header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,32 +1,22 @@
 import React from "react";
 import "../css/app.css";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link, useLocation } from "react-router-dom";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { RippleBadge } from "./MaterialTheme/styled";
 import {HomePage} from "./screens/homePage/index";
 import { OrdersPage } from "./screens/orderPage";
 import { ProductsPage } from "./screens/productsPage";
 import { UserPage } from "./screens/userPage";
+import { HomeNavbar } from "../app/components/headers/HomeNavbar";
+import { OtherNavbar } from "../app/components/headers/OtherNavbar";
+import { Footer } from "./components/footer";
 function App() {
+  const location = useLocation()
+  console.log ("location:", location)
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/products">Products</Link>
-          </li>
-          <li>
-            <Link to="/orders">Orders</Link>
-          </li>
-          <li>
-            <Link to="/member-page">UserPage</Link>
-          </li>
-          <li>
-            <Link to="/">HomePage</Link>
-          </li>
-        </ul>
-      </nav>
-      
+    <>
+     
+      {location.pathname === "/" ? <HomeNavbar/> : <OtherNavbar />}
       <Switch>
         <Route path="/products">
           <ProductsPage />
@@ -41,7 +31,8 @@ function App() {
           <HomePage />
         </Route>
       </Switch>
-    </div>
+      <Footer/>
+    </>
 );
 }
 
