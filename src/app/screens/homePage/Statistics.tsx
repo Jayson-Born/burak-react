8eab6d5fd5d2aa5ed874ed63f86195bcfbdcd9af
Jayson-Born <abdukabir34@gmail.com>
2025-01-18 00:26:54 +0900

    fix: modify  statistics sectional  component

--- src/app/screens/homePage/Statistics.tsx
+++ src/app/screens/homePage/Statistics.tsx
@@ -1,12 +1,39 @@
-import { Container } from "@mui/material";
+import { Box, Container, Divider, Stack } from "@mui/material";
 import React from "react";
 
 export default function Statistics(){
     return(
-        <div>
+        <div className={"static-frame"}>
             <Container>
-                Statistics
+               <Stack className= "info">
+               <Stack className="static-box">
+                    <Box className="static-num">12</Box>
+                    <Box className="static-text">Restaurant</Box>
+                </Stack>
+
+                <Divider sx={{ height: "64px", width: "2px", backgroundColor: "#E3C08D" }}/>
+
+                <Stack className="static-box">
+                    <Box className="static-num">8</Box>
+                    <Box className="static-text">Expreience</Box>
+                </Stack>
+
+                <Divider sx={{ height: "64px", width: "2px", backgroundColor: "#E3C08D" }}/>
+
+                <Stack className="static-box">
+                    <Box className="static-num">50+</Box>
+                    <Box className="static-text">Menu</Box>
+                </Stack>
+
+                <Divider sx={{ height: "64px", width: "2px", backgroundColor: "#E3C08D" }}/>
+
+                <Stack className="static-box">
+                    <Box className="static-num">200+</Box>
+                    <Box className="static-text">Client</Box>
+                </Stack>
+
+               </Stack>
             </Container>
         </div>
-    )
+    );
 }
