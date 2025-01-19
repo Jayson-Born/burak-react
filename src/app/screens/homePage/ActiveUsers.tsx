0c78042d74cb5c3e846bee353f21b8bb771ca31e
Jayson-Born <abdukabir34@gmail.com>
2025-01-19 18:05:00 +0900

    feat: develop ActiveUsers and Events sectional components

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -1,10 +1,67 @@
-import Container from "@mui/material/Container/Container";
+
 import React from "react";
+import CardOverflow from "@mui/joy/CardOverflow";
+import {CssVarsProvider,Container, Typography, Box, Stack}  from "@mui/joy";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import AdpectRatio from "@mui/joy/AspectRatio"
+import Card from "@mui/joy/Card"
+import Divider from "../../components/divider";
+ 
+const activeUsers = [
+    {memberNick: "Martin", memberImage: "/img/martin.webp"},
+    {memberNick: "Justin", memberImage: "/img/justin.webp"},
+    {memberNick: "Rose", memberImage: "/img/rose.webp"},
+    {memberNick: "Nusret", memberImage: "/img/nusret.webp"},
+]
 
 export default function ActiveUsers(){
+
     return (
-    <div>
-        <Container>ActiveUsers</Container>
+    <div  className={"active-users-frame"}>
+        <Container>
+            <Stack  className={"main"}>
+                <Box className={"category-title"}>Active Users</Box>
+                <Stack className={"cards-frame"}>
+                    <CssVarsProvider>
+                        {activeUsers.length !== 0 ? (
+                            activeUsers.map((ele, index) => {
+                                return(
+                                    <Card  key= {index} variant="outlined" className={"card"}>
+
+                       
+                        
+                                    <CardOverflow>
+                                    <AdpectRatio ratio="1">
+                                        <img src={ele.memberImage} alt="" />
+                                    </AdpectRatio>
+                                    </CardOverflow>
+            
+                                    <CardOverflow variant="soft" className="member-nickname">
+                                        <Stack className="info">
+                                            <Stack flexDirection={"row"}>
+                                                <Typography className={"title"}>
+                                                    {ele.memberNick}
+                                                </Typography>
+                                               
+                                            </Stack>
+                                            <Stack>
+                                               
+                                            </Stack>
+                                        </Stack>
+            
+                                    </CardOverflow>
+                                </Card>
+
+                                )
+                            })
+                            
+                        ) : (
+                            <Box className="no-data">New products are not available!</Box>
+                        )}
+                    </CssVarsProvider>
+                </Stack>
+            </Stack>
+        </Container>
     </div>
     )
 }
